b83129d48c44cfa9e99e0f99fc7d5835e45cd628
Tadeo <teoleon2013@gmail.com>
2025-09-01 13:55:47 -0300

    fiummba

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,15 +16,13 @@
 
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div class="sm:col-span-4">
-                <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                <x-form-lable for="title">Title</x-form-lable>
                 <div class="mt-2">
                 <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                 <input id="title" type="text" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
 
             </div>
-                @error('title')
-                    <p class="text-xs text-red-500 font-semibold mt-2">{{$message}}</p>
-                @enderror
+                <x-form-error name="title"/>
             <div class="sm:col-span-4 mt-4">
                 <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                 <div class="mt-2">
